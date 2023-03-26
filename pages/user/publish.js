6193949e60ca2e1177414c5657e7bb3653da435e
Lucas Gabriel Henschel <lucas101henschel@gmail.com>
2023-03-26 11:46:47 -0300

    Implemented price box

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -2,10 +2,14 @@ import {
   Box,
   Button,
   Container,
+  FormControl,
   IconButton,
   Select,
   TextField,
   Typography,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
 } from "@material-ui/core";
 import { useDropzone } from "react-dropzone";
 import { makeStyles } from "@material-ui/core/styles";
@@ -225,6 +229,23 @@ const Publish = () => {
           </Typography>
           <TextField multiline minRows={6} variant="outlined" fullWidth />
         </Box>
+      </Container>
+
+      <Container maxWidth="md" className={classes.boxContainer}>
+        <Box className={classes.box}>
+          <Typography component="h6" variant="h6" color="textPrimary">
+            Preço
+          </Typography>
+          <br />
+          <FormControl fullWidth variant="outlined">
+            <InputLabel>Valor</InputLabel>
+            <OutlinedInput 
+              onChange={() => {}}
+              startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+              labelWidth={40}
+            />      
+          </FormControl>
+        </Box>            
       </Container>
 
       <Container maxWidth="md" className={classes.boxContainer}>
