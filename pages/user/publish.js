84f9605bd6305dc853c912c1f3c3dc6951a8a543
Lucas Gabriel Henschel <lucas101henschel@gmail.com>
2023-03-25 23:03:41 -0300

    Implemented image upload

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -1,9 +1,23 @@
-import { Box, Button, Container, Select, TextField, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Select,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { useDropzone } from "react-dropzone";
+import { makeStyles } from "@material-ui/core/styles";
+import { DeleteForever } from "@material-ui/icons";
 
-import TemplateDefault from '../../src/templates/Default';
+import TemplateDefault from "../../src/templates/Default";
+import { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
+  mask: {},
+  mainImage: {},
+
   container: {
     padding: theme.spacing(8, 0, 6),
   },
@@ -16,34 +30,117 @@ const useStyles = makeStyles((theme) => ({
   boxContainer: {
     paddingBottom: theme.spacing(3),
   },
-}))
+
+  thumbsContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    marginTop: 15,
+  },
+
+  dropzone: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    padding: 10,
+    width: 200,
+    height: 150,
+    margin: "0 15px 15px 0",
+    backgroundColor: theme.palette.background.default,
+    border: "2px dashed black",
+  },
+
+  thumb: {
+    position: "relative",
+    width: 200,
+    height: 150,
+    backgroundSize: "cover",
+    margin: '0 15px 15px 0',
+    backgroundPosition: "center center",
+
+    "& $mainImage": {
+      backgroundColor: "blue",
+      padding: "6px 10px",
+      position: "absolute",
+      bottom: 0,
+      left: 0,
+    },
+
+    "&:hover $mask": {
+      display: "flex",
+    },
+
+    "& $mask": {
+      display: "none",
+      justifyContent: "center",
+      alignItems: "center",
+      textAlign: "center",
+      backgroundColor: "rgba(0, 0, 0, 0.7)",
+      width: "100%",
+      height: "100%",
+    },
+  },
+}));
 
 const Publish = () => {
   const classes = useStyles();
+  const [files, setFiles] = useState([]);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFile) => {
+      const newFiles = acceptedFile.map((file) => {
+        return Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        });
+      });
+
+      setFiles([
+        ...files, 
+        ...newFiles
+      ]);
+    },
+  });
+
+  const handleRemoveFile = (fileName) => {
+    const newFileState = files.filter(file => file.name !== fileName);
+    setFiles(newFileState);  
+  }
 
   return (
     <TemplateDefault>
       <Container maxWidth="sm" className={classes.container}>
-        <Typography component="h1" variant="h2" align="center" color="textPrimary">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+        >
           Publicar Anúncio
         </Typography>
-        <Typography component="h5" variant="h5" align="center" color="textPrimary">
+        <Typography
+          component="h5"
+          variant="h5"
+          align="center"
+          color="textPrimary"
+        >
           Quanto mais detalhado, melhor!
         </Typography>
-      </Container>  
+      </Container>
 
       <Container maxWidth="md" className={classes.boxContainer}>
         <Box className={classes.box}>
           <Typography component="h6" variant="h6" color="textPrimary">
             Título do Anúncio
           </Typography>
-          <TextField 
+          <TextField
             label="ex.: Bicicleta Aro 18 com garantia"
             size="small"
             fullWidth
           />
 
-          <br /><br />
+          <br />
+          <br />
           <Typography component="h6" variant="h6" color="textPrimary">
             Categoria
           </Typography>
@@ -53,7 +150,7 @@ const Publish = () => {
             fullWidth
             onChange={() => {}}
             inputProps={{
-              name: 'age',
+              name: "age",
             }}
           >
             <option value="">Selecione</option>
@@ -84,7 +181,38 @@ const Publish = () => {
           <Typography component="div" variant="body2" color="textPrimary">
             A primeira imagem é a foto principal do seu anúncio.
           </Typography>
-        </Box>      
+          <Box className={classes.thumbsContainer}>
+            <Box className={classes.dropzone} {...getRootProps()}>
+              <input {...getInputProps()} />
+              <Typography variant="body2" color="textPrimary">
+                Clique para adicionar ou arraste a imagem para aqui.
+              </Typography>
+            </Box>
+
+            {files.map((file, index) => (
+              <Box
+                key={file.name}
+                className={classes.thumb}
+                style={{ backgroundImage: `url(${file.preview})` }}
+              >
+                {
+                  index === 0 ?
+                    <Box className={classes.mainImage}>
+                      <Typography variant="body2" color="secondary"> 
+                        Principal
+                      </Typography>
+                    </Box>
+                  : null  
+                }
+                <Box className={classes.mask}>
+                  <IconButton color="secondary" onClick={() => handleRemoveFile(file.name)}>
+                    <DeleteForever fontSize="large" />
+                  </IconButton>
+                </Box>
+              </Box>
+            ))}
+          </Box>
+        </Box>
       </Container>
 
       <Container maxWidth="md" className={classes.boxContainer}>
@@ -95,53 +223,46 @@ const Publish = () => {
           <Typography component="div" variant="body2" color="textPrimary">
             Escreva os detalhes do que está vendendo
           </Typography>
-          <TextField 
-            multiline
-            rows={6}
-            variant="outlined"
-            fullWidth
-          />
-        </Box>      
+          <TextField multiline minRows={6} variant="outlined" fullWidth />
+        </Box>
       </Container>
 
       <Container maxWidth="md" className={classes.boxContainer}>
         <Box className={classes.box}>
-          <Typography component="h6" variant="h6" color="textPrimary" gutterBottom>
+          <Typography
+            component="h6"
+            variant="h6"
+            color="textPrimary"
+            gutterBottom
+          >
             Dados de contato
           </Typography>
-          <TextField 
-            label="Nome"
-            variant="outlined"
-            size="small"
-            fullWidth
-          />
-          <br /><br />
-          <TextField 
-            label="E-mail"
-            variant="outlined"
-            size="small"
-            fullWidth
-          />
-          <br /><br />
-          <TextField 
+          <TextField label="Nome" variant="outlined" size="small" fullWidth />
+          <br />
+          <br />
+          <TextField label="E-mail" variant="outlined" size="small" fullWidth />
+          <br />
+          <br />
+          <TextField
             label="Telefone"
             variant="outlined"
             size="small"
             fullWidth
           />
-          <br /><br />
-        </Box>      
+          <br />
+          <br />
+        </Box>
       </Container>
 
       <Container maxWidth="md" className={classes.boxContainer}>
         <Box textAlign="right">
-          <Button variant='contained' color="primary"> 
+          <Button variant="contained" color="primary">
             Publicar anúncio
           </Button>
         </Box>
       </Container>
-    </TemplateDefault>  
-  )    
-}
+    </TemplateDefault>
+  );
+};
 
 export default Publish;
